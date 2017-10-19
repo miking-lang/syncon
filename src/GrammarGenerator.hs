23c2efdb76175a8600a3e9f5db1507d3dd440caa
Viktor Palmkvist <viktor@palmkvist.com>
2017-10-19 15:35:28 +0200

    Associativity

--- src/GrammarGenerator.hs
+++ src/GrammarGenerator.hs
@@ -5,9 +5,10 @@ module GrammarGenerator (parseGrammar, Node(..), MidNode(..)) where
 import Data.Function ((&), on)
 import Data.List (groupBy, sortBy, intercalate)
 import Data.Maybe (mapMaybe, maybeToList)
+import Data.Traversable (mapAccumL, mapAccumR)
 import Data.Foldable (asum, foldlM)
 import Control.Applicative (some, many, optional)
-import Control.Arrow ((&&&))
+import Control.Arrow ((&&&), first, second)
 import Control.Monad.Fix (mfix)
 import qualified Data.Map as M
 
@@ -53,41 +54,57 @@ generateType (currentTy, constructions) nonTerminals =
       generateConstruction nonTerminals thisLevel nextLevel <$> level
         & (nextLevel:) & asum & rule
     generateConstruction :: M.Map String (Production r Node) -> Production r Node -> Production r Node -> Construction -> Production r Node
-    generateConstruction nonTerminals thisLevel nextLevel Construction{name, syntaxType, syntax} = -- TODO: assoc
-      uncurry (Node name) <$> sequencePats syntax
+    generateConstruction nonTerminals thisLevel nextLevel Construction{name, syntaxType, syntax, extraData} =
+      uncurry (Node name) <$> case assocData extraData of
+        Just _ -> snd $ sequencePats OnceThis syntax
+        Nothing -> snd $ sequencePats AlwaysThis syntax
       where
-        toProd (NamedPat n pat) = (Just n,) <$> toProd' pat
-        toProd a = (Nothing,) <$> toProd' a
-        toProd' (NamedPat _ pat) = toProd' pat
-        toProd' IdentifierPat = identifier
-        toProd' (TokenPat tok) = Basic <$> satisfy (sameContent tok) <?> show tok
-        toProd' IntegerPat = integer
-        toProd' FloatPat = float
-        toProd' StringPat = string
-        toProd' (SyntaxPat p)
-          | p == currentTy = MidNode <$> thisLevel <?> p -- TODO: assoc
-          | otherwise = MidNode <$> nonTerminals M.! p <?> p
-        toProd' (RepeatPat pat rep) = Repeated rep <$> case rep of
-          StarRep -> many $ toProd' pat
-          PlusRep -> some $ toProd' pat
-          QuestionRep -> fmap maybeToList . optional $ toProd' pat
-        toProd' (SequencePat pats) = uncurry Sequenced <$> sequencePats pats
-        sequencePats pats = toProd <$> pats
-          & sequenceA
-          & fmap namedAndRange
+        sequencePats acc pats = mapAccum toProd acc pats
+          & second (fmap namedAndRange . sequenceA)
+        mapAccum = case assocData extraData of
+          Just AssocRight -> mapAccumR
+          _ -> mapAccumL
         namedAndRange children =
           let r = mconcat $ range . snd <$> children
               named = mapMaybe (\(n, mn) -> (, mn) <$> n) children
           in (named, r)
+        toProd acc = \case
+          IdentifierPat -> basic acc identifier
+          IntegerPat -> basic acc integer
+          FloatPat -> basic acc float
+          StringPat -> basic acc string
+          TokenPat tok -> (acc,) $ (Nothing,) . Basic <$> satisfy (sameContent tok) <?> show tok
+          NamedPat n pat -> Just n & const & first & fmap & second $ toProd acc pat
+          SequencePat pats -> (Nothing,) . uncurry Sequenced & fmap & second $ sequencePats acc pats
+          RepeatPat pat rep -> (Nothing,) . Repeated rep & fmap & second $ case rep of
+            StarRep -> second (many . fmap snd) $ toProd acc pat
+            PlusRep -> second (some . fmap snd) $ toProd acc pat
+            QuestionRep -> second (fmap maybeToList . optional . fmap snd) $ toProd acc pat
+          SyntaxPat pat ->
+            let (acc', prod) = if pat == currentTy
+                then (nextAssoc acc, if this acc then thisLevel else nextLevel)
+                else (acc, nonTerminals M.! pat)
+            in (acc', (Nothing,) . MidNode <$> prod <?> pat)
+        basic acc = (acc,) . fmap ((Nothing,) . Basic)
 
-integer :: Production r MidNode
-integer = Basic <$> satisfy (\case { IntegerTok{} -> True; _ -> False }) <?> "integer"
-float :: Production r MidNode
-float = Basic <$> satisfy (\case { FloatTok{} -> True; _ -> False }) <?> "float"
-string :: Production r MidNode
-string = Basic <$> satisfy (\case { StringTok{} -> True; _ -> False }) <?> "string"
-identifier :: Production r MidNode
-identifier = Basic <$> satisfy (\case { IdentifierTok{} -> True; _ -> False }) <?> "identifier"
+data AssocState = AlwaysThis | OnceThis | NeverThis deriving (Eq)
+
+nextAssoc :: AssocState -> AssocState
+nextAssoc AlwaysThis = AlwaysThis
+nextAssoc OnceThis = NeverThis
+nextAssoc NeverThis = NeverThis
+
+this :: AssocState -> Bool
+this = (/= NeverThis)
+
+integer :: Production r Token
+integer = satisfy (\case { IntegerTok{} -> True; _ -> False }) <?> "integer"
+float :: Production r Token
+float = satisfy (\case { FloatTok{} -> True; _ -> False }) <?> "float"
+string :: Production r Token
+string = satisfy (\case { StringTok{} -> True; _ -> False }) <?> "string"
+identifier :: Production r Token
+identifier = satisfy (\case { IdentifierTok{} -> True; _ -> False }) <?> "identifier"
 
 instance Show Node where
   show Node{name, children, nodeRange} = name ++ "{" ++ show nodeRange ++ "}" ++ showNamed children
