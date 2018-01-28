b3147c9b2f570129f31c8fcef5a3f5de50eb7d01
Viktor Palmkvist <viktor@palmkvist.com>
2018-01-28 11:46:48 +0100

    Ambiguity reports two levels now, makes it a little bit more intuitive

--- src/Ambiguity.hs
+++ src/Ambiguity.hs
@@ -1,23 +1,25 @@
-module Ambiguity (ambiguities) where
+module Ambiguity (ambiguities, Repr) where
 
 import Data.List (transpose, nub)
+import Data.Char (isAlphaNum)
 import Control.Arrow ((&&&))
 
-import Types.Lexer (Ranged(..), Range, Token(..))
+import Types.Lexer (Ranged(..), Range)
 import Types.Construction (Repeat(..))
 import Types.Ast (NodeI(Node, name, SyntaxSplice), MidNodeI(..), FixNode(..))
 
 type Node s = FixNode s String
 type MidNode s = MidNodeI s String
 
-ambiguities :: [Node s] -> [(Range, [[SimpleRepr]])]
-ambiguities nodes = ambig $ midWrap <$> nodes
+ambiguities :: [Node s] -> [(Range, [Repr])]
+ambiguities nodes = ambig $ wrap <$> nodes
   where
     r = range nodes
-    midWrap (FixNode n) = MidNode $ Node "*top*" [MidNode n] r
+    wrap (FixNode n) = midWrap $ MidNode n
+    midWrap m = MidNode $ Node "*wrap*" [m] (range m)
 
 -- invariant: all items in the argument list share a SimpleRepr
-ambig :: [MidNode s] -> [(Range, [[SimpleRepr]])]
+ambig :: [MidNode s] -> [(Range, [Repr])]
 ambig [] = []
 ambig nodes@(node:_) =
   let cs = children <$> nodes
@@ -26,25 +28,36 @@ ambig nodes@(node:_) =
       similarColumns = allEq <$> transpose simpleCs
       nonSimilar = keepOnly (not <$> similarColumns) <$> simpleCs
       similar = keepOnly similarColumns <$> cs
+      conflictingReprs = case filter not similarColumns of
+        [] -> []
+        [_] -> toRepr . head . keepOnly (not <$> similarColumns) <$> cs
+        _ -> toRepr <$> nodes
   in case node of
-       Repeated{} | not childLengthsEqual -> return $ singleton simpleCs
-       _ | null $ head nonSimilar -> concat $ ambig <$> transpose similar
-       _ -> singleton nonSimilar : concat (ambig <$> transpose similar)
+       Repeated{} | not childLengthsEqual -> return . singleton $ toRepr <$> nodes
+       _ | and similarColumns -> concat $ ambig <$> transpose similar
+       _ -> singleton conflictingReprs : concat (ambig <$> transpose similar)
   where
     singleton = range &&& nub
 
 toSimple :: MidNode s -> SimpleRepr
-toSimple (MidNode n@Node{name}) = SimpleRepr (NodeTag name) (range n)
-toSimple (MidNode SyntaxSplice{}) = SimpleRepr SpliceTag mempty
-toSimple (MidIdentifier r i) = SimpleRepr IdentifierTag r
-toSimple (MidSplice _) = SimpleRepr SpliceTag mempty
-toSimple (Basic t@IdentifierTok{}) = SimpleRepr IdentifierTag (range t)
-toSimple (Basic t@SymbolTok{}) = SimpleRepr SymbolTag (range t)
-toSimple (Basic t@StringTok{}) = SimpleRepr StringTag (range t)
-toSimple (Basic t@IntegerTok{}) = SimpleRepr IntegerTag (range t)
-toSimple (Basic t@FloatTok{}) = SimpleRepr FloatTag (range t)
-toSimple n@(Repeated r _) = SimpleRepr (RepeatTag r) (range n)
-toSimple (Sequenced r _) = SimpleRepr SequencedTag r
+toSimple m = SimpleRepr (toTag m) (range m)
+
+toRepr :: MidNode s -> Repr
+toRepr m =
+  Repr (toTag m)
+       (filter (isInteresting . fst) $ (toTag &&& range) <$> children m)
+       (range m)
+  where
+    isInteresting = (/= BasicTag)
+
+toTag :: MidNode s -> Tag
+toTag (MidNode Node{name}) = NodeTag name
+toTag (MidNode SyntaxSplice{}) = SpliceTag
+toTag MidIdentifier{} = IdentifierTag
+toTag (MidSplice _) = SpliceTag
+toTag Basic{} = BasicTag
+toTag (Repeated rep _) = (RepeatTag rep)
+toTag Sequenced{} = SequencedTag
 
 allEq :: Eq a => [a] -> Bool
 allEq [] = True
@@ -64,17 +77,36 @@ children Basic{} = []
 children (Repeated _ cs) = cs
 children (Sequenced _ cs) = cs
 
+data Repr = Repr Tag [(Tag, Range)] Range deriving (Show, Eq)
+
 data SimpleRepr = SimpleRepr Tag Range deriving (Show, Eq)
 data Tag = NodeTag String
          | IdentifierTag
          | SpliceTag
-         | SymbolTag
-         | StringTag
-         | IntegerTag
-         | FloatTag
+         | BasicTag
          | RepeatTag Repeat
          | SequencedTag
-         deriving (Show, Eq)
+         deriving (Eq)
 
+instance Ranged Repr where
+  range (Repr _ _ r) = r
 instance Ranged SimpleRepr where
   range (SimpleRepr _ r) = r
+
+instance Show Tag where
+  show (NodeTag name) = case break (== '#') name of
+    (name, []) -> name
+    ([], name) -> name
+    (p : prefix, name) -> p : formatPrefix prefix ++ name
+    where
+      formatPrefix [] = ""
+      formatPrefix (s : n : rest)
+        | not $ isAlphaNum s = s : n : formatPrefix rest
+      formatPrefix (_ : rest) = formatPrefix rest
+  show IdentifierTag{} = "identifier"
+  show SpliceTag{} = "splice"
+  show BasicTag{} = "token"
+  show (RepeatTag StarRep) = "rep*"
+  show (RepeatTag PlusRep) = "rep+"
+  show (RepeatTag QuestionRep) = "rep?"
+  show SequencedTag{} = "sequence"
