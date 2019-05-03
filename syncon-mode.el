58234480fe87ef28ee071dad84ec9ca2e289e6a9
Elias Castegren <elias.castegren@gmail.com>
2019-05-03 11:33:35 +0200

    Cleaned up the Emacs mode
    Summary of changes:
    
    * Sorted keyword list
    * Removed unused syntax classes
    * Cleaned up syntax table (`{/ this is no longer a block comment /}`)
    * Added Elisp start/end comments
    
    In other words, no new features

--- syncon-mode.el
+++ syncon-mode.el
@@ -1,36 +1,27 @@
+;;; syncon-mode.el
+
 ;;;;;;;;;;;;;;;;;;
 ;; Highlighting ;;
 ;;;;;;;;;;;;;;;;;;
 
-;; Please keep these lists sorted
+;; Please keep this list sorted
 (setq syncon-keywords
      '(
-       "token"
-       "syncon"
-       "prefix"
-       "postfix"
-       "infix"
-       "comment"
-       "left"
-       "right"
-       "precedence"
-       "except"
-       "type"
        "builtin"
+       "comment"
+       "except"
        "forbid"
+       "infix"
+       "left"
+       "postfix"
+       "precedence"
+       "prefix"
        "rec"
+       "right"
+       "syncon"
+       "token"
+       "type"
       ))
-
-(setq syncon-danger-words
-     '(
-       ))
-(setq syncon-constants
-     '(
-       ))
-
-(setq syncon-primitives
-     '(
-       ))
 
 (setq syncon-operators
      '(
@@ -41,29 +32,15 @@
        ))
 
 (setq syncon-keywords-regexp (regexp-opt syncon-keywords 'symbols))
-(setq syncon-danger-regexp (regexp-opt syncon-danger-words 'symbols))
-(setq syncon-constants-regexp (regexp-opt syncon-constants 'symbols))
-(setq syncon-primitives-regexp (regexp-opt syncon-primitives 'symbols))
 (setq syncon-operators-regexp (regexp-opt syncon-operators 'symbols))
 
-(setq syncon-types-regexp "\\<[A-Z][a-zA-Z]*\\>")
-;; (setq syncon-function-regexp “\\<\\(def\\|stream\\|fun\\|require\\)\\> \\([^(]*\\)([^)]*)\\W*:\\W*.*“)
-;; (setq syncon-variable-regexp “\\<\\([A-Za-z0-9_]*\\)\\>\\W*:“)
-(setq syncon-comment-regexp "//.?*")
-;; (setq syncon-char-regexp “‘\\(\\\\.\\|.\\)‘“)
+(setq syncon-types-regexp "\\<[[:upper:]][[:word:]]*\\>")
 
 (setq syncon-font-lock-keywords
      `(
-       (,syncon-comment-regexp    . font-lock-comment-face)
-       ;; (,syncon-char-regexp       . font-lock-string-face)
        (,syncon-keywords-regexp   . font-lock-keyword-face)
-       (,syncon-danger-regexp     . font-lock-warning-face)
-       (,syncon-constants-regexp  . font-lock-constant-face)
-       (,syncon-primitives-regexp . font-lock-type-face)
        (,syncon-operators-regexp  . font-lock-builtin-face)
        (,syncon-types-regexp      . font-lock-type-face)
-       ;; (,syncon-function-regexp   2 font-lock-function-name-face)
-       ;; (,syncon-variable-regexp   1 font-lock-variable-name-face)
        )
      )
 
@@ -71,11 +48,8 @@
 
 (setq syncon-mode-syntax-table
      (let ( (synTable (make-syntax-table)))
-       ;; Block comment “{- ... -}”
-       ;; Inline comment “-- ...”
-       (modify-syntax-entry ?\{ "(}1nb" synTable)
-       (modify-syntax-entry ?\} "){4nb" synTable)
-       (modify-syntax-entry ?/ "_ 123" synTable)
+       ;; Inline comment “// ...”
+       (modify-syntax-entry ?/ ". 12a" synTable)
        (modify-syntax-entry ?\n ">" synTable)
        synTable))
 
@@ -92,3 +66,4 @@
 (add-to-list 'auto-mode-alist '("\\.syncon\\'" . syncon-mode))
 
 (provide 'syncon-mode)
+;;; syncon-mode.el ends here
