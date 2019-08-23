9616b26c91fa668fadb1e8038443044cbf139856
Viktor Palmkvist <viktor@palmkvist.com>
2019-08-23 13:43:50 +0200

    Ripped out things not relevant to the current project

--- syncon-parser/gll/src/GLL/Combinators.hs
+++
@@ -1,8 +0,0 @@
-
--- |
--- "GLL.Combinators" imports "GLL.Combinators.Interface" as the default library.
-module GLL.Combinators (
-    module GLL.Combinators.Interface 
-    ) where
-
-import GLL.Combinators.Interface
